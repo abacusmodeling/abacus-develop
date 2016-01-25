31aefef672f8a34435c161b36e0de1903379a9fa
Liu Xiaohui <liuxiaoh@mail.ustc.edu.cn>
2016-01-25 20:20:21 +0800

    twice the number of kmesh in making overlap table for high precision

--- ABACUS.1.0.0/source/src_lcao/lcao_orbitals.cpp
+++ ABACUS.1.0.0/source/src_lcao/lcao_orbitals.cpp
@@ -149,7 +149,9 @@ void LCAO_Orbitals::Read_Orbitals(void)
 	// calculate number of k mesh according to energy cutoff.
 	// Mohan choose ecutwfc according to interpolation requirement.
 	//	cout << " ecutwfc=" << ecutwfc << endl;
-	this->kmesh = static_cast<int>( sqrt(ecutwfc) / dk )  + 4;
+	//LiuXh modified 2016-01-25
+	//this->kmesh = static_cast<int>( sqrt(ecutwfc) / dk )  + 4;
+	this->kmesh = static_cast<int>( 2 * sqrt(ecutwfc) / dk )  + 4;
 	//	this->kmesh = static_cast<int> (PI / 0.01 / 4 / this->dk);
 	if(kmesh%2==0) kmesh++;
 	OUT(ofs_running,"kmesh",kmesh);
