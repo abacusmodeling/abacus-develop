152b042b8866ed6b493edfdb86abd4e736453842
mohan <mohan.chen.chen.mohan@gmail.com>
2021-03-24 14:39:32 +0800

    fixed time recording bug in pw line and jiyy input bug

--- ABACUS.develop/source/input.cpp
+++ ABACUS.develop/source/input.cpp
@@ -1484,7 +1484,7 @@ bool Input::Read(const string &fn)
 		else if (strcmp("ocp_set", word) == 0)
 		{
 			getline(ifs, ocp_set);
-			ifs.ignore(150, '\n');
+//			ifs.ignore(150, '\n');
 		}
         // else if (strcmp("ocp_n", word) == 0)
         // {
