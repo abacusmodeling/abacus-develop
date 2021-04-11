e81da4dd12fb453ead311453146914df7b614a1c
CY_fortneu <33978601+maki49@users.noreply.github.com>
2021-04-11 16:16:17 +0800

    Revert "fix bugs in lcao-line descriptor"

--- ABACUS.develop/source/src_lcao/ORB_table_alpha.h
+++ ABACUS.develop/source/src_lcao/ORB_table_alpha.h
@@ -1,5 +1,5 @@
-#ifndef ORB_TABLE_ALPHA_H
-#define ORB_TABLE_ALPHA_H
+#ifndef ORB_TABLE_ALPHA_H 
+#define ORB_TABLE_ALPHA_H 
 
 #include "ORB_atomic_lm.h"
 #include "../src_global/sph_bessel_recursive.h"
@@ -8,19 +8,20 @@
 
 class ORB_table_alpha
 {
-public:
+	public:
+
 	ORB_table_alpha();
 	~ORB_table_alpha();
 
 	void allocate(
-		const int &ntype,
-		const int &lmax_in,
-		const int &kmesh_in,
-		const double &Rmax_in,
-		const double &dR_in,
-		const double &dk_in);
+		const int& ntype,
+		const int& lmax_in,
+		const int& kmesh_in,
+		const double& Rmax_in,
+		const double& dR_in,
+		const double& dk_in);
 
-	double *****Table_DSR; //overlap between lcao basis phi and descriptor basis alpha
+	double***** Table_DSR;//overlap between lcao basis phi and descriptor basis alpha
 	bool destroy_nr;
 
 	//-------------------------
@@ -30,39 +31,40 @@ public:
 	void init_DS_Opair(void);
 	void init_DS_2Lplus1(void);
 	IntArray DS_Opair;
-	int *DS_2Lplus1;
+	int* DS_2Lplus1;
 
-	void init_Table_Alpha(Sph_Bessel_Recursive::D2 *pSB);
+	void init_Table_Alpha(Sph_Bessel_Recursive::D2* pSB);
 
 	void Destroy_Table_Alpha(void);
 
-	static int get_rmesh(const double &R1, const double &R2);
+	static int get_rmesh(const double& R1, const double& R2);
 
 	static double dr;
 	int Rmesh;
 	int ntype;
 	int lmax;
 
-	//void print_Table_DSR(void);		//caoyu add 2021-03-20
+	void print_Table_DSR(void);		//caoyu add 2021-03-20
 
-private:
+	private:
+
 	void cal_S_PhiAlpha_R(
-		Sph_Bessel_Recursive::D2 *pSB, // mohan add 2021-03-06
-		const int &l,
-		const Numerical_Orbital_Lm &n1,
-		const Numerical_Orbital_Lm &n2,
-		const int &rmesh,
-		double *rs,
-		double *drs);
+		Sph_Bessel_Recursive::D2* pSB, // mohan add 2021-03-06
+		const int& l,	
+		const Numerical_Orbital_Lm& n1,
+		const Numerical_Orbital_Lm& n2,
+		const int& rmesh,
+		double* rs,
+		double* drs);
 
 	// variables
 	double Rmax;
 	double dk;
 	int nlm;
 	int kmesh;
-	double *kpoint;
-	double *r;
-	double *rab;
-	double *kab;
+	double* kpoint;
+	double* r;
+	double* rab;
+	double* kab;
 };
 #endif
