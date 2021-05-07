e7d1954f634b330d10522b3a3e542e3ac18070f2
maki49 <1579492865@qq.com>
2021-05-07 19:02:14 +0800

    deepks: divide S_mu_alpha and PDM into small matrices with same I,n,l

--- ABACUS.develop/source/src_lcao/LCAO_descriptor.h
+++ ABACUS.develop/source/src_lcao/LCAO_descriptor.h
@@ -9,7 +9,7 @@ class LCAO_Descriptor
 {
 public:
 
-    LCAO_Descriptor();
+    LCAO_Descriptor(int lm, int inlm);
     ~LCAO_Descriptor();
 
 	// cal S_alpha_mu: overlap between lcao basis Phi and descriptor basis Al
@@ -25,10 +25,10 @@ public:
 private:
 
 	// overlap between lcao and descriptor basis
-    double *S_mu_alpha;
+    double **S_mu_alpha;	//[Alpha.totnchi][NLOCAL*(2*lmax+1)]	caoyu modified 2021-05-07
 
 	// projected density matrix
-    double *PDM;
+    double **PDM;	//[2l+1][2l+1]	caoyu modified 2021-05-07
 
 	// descriptors
     double *d;
@@ -36,16 +36,23 @@ private:
     int n_descriptor;
 
 	// \sum_L{Nchi(L)*(2L+1)}
-    int des_per_atom;
+	int des_per_atom;
 
-    IntArray *mu_index;
+	const int lmaxd;
+
+	const int inlmax;
+
+	IntArray* mu_index;
+	IntArray* inl_index;	//caoyu add 2021-05-07
+	int* inl_ld;	//inl_ld[inl_index] = l_descriptor
 
     void init_mu_index(void);
     
 	void set_S_mu_alpha(
 		const int &iw1_all, 
-		const int &iw2_all, 
-		const double &v);
+		const int& inl,
+		const int& im,
+		const double& v);
 
     void print_projected_DM(
 		ofstream &ofs, 
