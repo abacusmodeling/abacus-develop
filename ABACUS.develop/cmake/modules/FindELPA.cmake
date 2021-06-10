22b8194b05108b5dd29690f6e57b14b6cb99da68
Shaochen Shi <shishaochen_ha@sina.com>
2021-06-10 20:21:27 +0800

    Find cereal, ELPA, FFTW3, ScaLAPACK in CMake.

---
+++ ABACUS.develop/cmake/modules/FindELPA.cmake
@@ -0,0 +1,33 @@
+###############################################################################
+# - Find ELPA
+# Find the native ELPA headers and libraries.
+#
+#  ELPA_FOUND        - True if libelpa is found.
+#  ELPA_LIBRARIES    - List of libraries when using libyaml
+#  ELPA_INCLUDE_DIRS - Where to find ELPA headers.
+#
+
+find_path(ELPA_INCLUDE_DIR elpa/elpa.h)
+find_library(ELPA_LIBRARY NAMES elpa)
+
+# Handle the QUIET and REQUIRED arguments and
+# set ELPA_FOUND to TRUE if all variables are non-zero.
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ELPA DEFAULT_MSG ELPA_LIBRARY ELPA_INCLUDE_DIR)
+
+# Copy the results to the output variables and target.
+if(ELPA_FOUND)
+    set(ELPA_LIBRARIES ${ELPA_LIBRARY})
+    set(ELPA_INCLUDE_DIRS ${ELPA_INCLUDE_DIR})
+
+    if(NOT TARGET ELPA::ELPA)
+        add_library(ELPA::ELPA UNKNOWN IMPORTED)
+        set_target_properties(ELPA::ELPA PROPERTIES
+           IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+           IMPORTED_LOCATION "${ELPA_LIBRARY}"
+           INTERFACE_INCLUDE_DIRECTORIES "${ELPA_INCLUDE_DIR}")
+    endif()
+endif()
+
+mark_as_advanced(ELPA_INCLUDE_DIR ELPA_LIBRARY)
+
