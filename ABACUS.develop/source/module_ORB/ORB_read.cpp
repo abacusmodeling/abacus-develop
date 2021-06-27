2845155541b98fb7a0767353a24a4175a7a68851
Liu-RX <75369672+Liu-RX@users.noreply.github.com>
2021-06-27 11:48:01 +0800

    Update ORB_read.cpp

--- ABACUS.develop/source/module_ORB/ORB_read.cpp
+++ ABACUS.develop/source/module_ORB/ORB_read.cpp
@@ -3,7 +3,6 @@
 #include <cassert>
 #include "../src_global/math_integral.h"
 #include "../src_global/tool_check.h"
-#include "../src_global/"
 #include <algorithm>
 //#include "src_pw/soc.h"
 using namespace std;
