8c5e4b9d6c47c8454a44beaf56b9c40675aa9b38
dyzheng <dyzheng@mail.ustc.edu.cn>
2021-08-16 14:36:54 +0000

    added warning quit when MD without atom can move

--- source/module_cell/read_atoms.cpp
+++ source/module_cell/read_atoms.cpp
@@ -698,6 +698,13 @@ bool UnitCell_pseudo::read_atom_positions(std::ifstream &ifpos, std::ofstream &o
 		}//end for ntype
 	}// end scan_begin
 
+//check if any atom can move in MD
+std::cout<<__FILE__<<__LINE__<<this->if_atoms_can_move()<<" "<<GlobalV::CALCULATION<<std::endl;
+	if(!this->if_atoms_can_move() && GlobalV::CALCULATION=="md")
+	{
+		WARNING("read_atoms", "no atom can move in MD!");
+		return 0;
+	} 
 
 	ofs_running << std::endl;
 	OUT(ofs_running,"TOTAL ATOM NUMBER",nat);
