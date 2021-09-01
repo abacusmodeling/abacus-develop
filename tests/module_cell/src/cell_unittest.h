9e548499745a144c254629f2b3161c750f0e7276
wenfei-li <liwenfei94@gmail.com>
2021-09-01 12:30:55 +0800

    module_cell : move unit test source files to tests

---
+++ tests/module_cell/src/cell_unittest.h
@@ -0,0 +1,47 @@
+#include "../../../source/module_cell/unitcell_pseudo.h"
+#include "../../../source/src_parallel/parallel_global.h"
+
+#ifdef __LCAO
+#include "../../module_orb/src/ORB_unittest.h"
+#include "../../../source/module_orbital/ORB_read.h" // to use 'ORB' -- mohan 2021-01-30
+#include "../../../source/module_neighbor/sltk_grid_driver.h"
+
+namespace Test_Cell
+{
+	extern Grid_Driver GridD;
+}
+
+class test_cell_orb : public test_orb
+#else
+class test_cell
+#endif
+{
+
+public:
+#ifdef __LCAO
+	test_cell_orb();
+	~test_cell_orb();
+#else
+	test_cell();
+	~test_cell();
+#endif
+
+	std::ofstream ofs_warning;
+	UnitCell_pseudo ucell;
+	output out;
+
+#ifndef __LCAO
+	std::ofstream ofs_running;
+	int ntype;
+#endif
+
+	void set_parameters();//set some global variables
+	void setup_cell();
+
+#ifdef __LCAO
+	void build_ST_new(const char& dtype, const bool& calc_deri);
+#else
+	void count_ntype(); //from STRU, count types of elements
+#endif
+};
+
---
+++ tests/module_cell/src/main.cpp
@@ -0,0 +1,42 @@
+//#include "timer.h"
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include "cell_unittest.h"
+
+using namespace std;
+
+void calculate();
+
+int main(int argc, char **argv)
+{
+
+	std::cout << "Hello, this is the cell module of ABACUS." << std::endl;
+
+	calculate();
+
+    return 0;
+}
+
+void calculate()
+{
+#ifdef __LCAO
+	test_cell_orb test;
+#else
+	test_cell test;
+#endif
+
+	test.count_ntype();
+	test.set_parameters();
+	test.setup_cell();
+
+#ifdef __LCAO
+	test.set_ekcut();
+	test.read_files();
+	test.build_ST_new('S',false);
+#endif
+    return;
+}
