f7ff51eb9e7474ac12dfbc0bdf232c2e628e82e5
HeFuxiang94 <78629482+HeFuxiang94@users.noreply.github.com>
2021-09-13 17:00:34 +0800

    Modify 2 bugs of TD.

--- source/src_lcao/LCAO_evolve.h
+++ source/src_lcao/LCAO_evolve.h
@@ -9,14 +9,14 @@ class Evolve_LCAO_Matrix
 	Evolve_LCAO_Matrix();
 	~Evolve_LCAO_Matrix();
 	
-	void evolve_complex_matrix(const int &ik, std::complex<double> **c, std::complex<double> **c_init)const;
+	void evolve_complex_matrix(const int &ik, std::complex<double> **c, ModuleBase::ComplexMatrix &wfc_2d)const;
 
 	private:
 
 	void using_LAPACK_complex(const int &ik, std::complex<double> **c, std::complex<double> **c_init)const;
 #ifdef __MPI
 	int using_ScaLAPACK_complex(const int &ik, std::complex<double>** c, std::complex<double>** c_init)const;
-	int using_ScaLAPACK_complex_3(const int &ik, std::complex<double>** c, std::complex<double>** c_init)const;
+	int using_ScaLAPACK_complex_3(const int &ik, std::complex<double>** c, ModuleBase::ComplexMatrix &wfc_2d)const;
 #endif
 };
 
