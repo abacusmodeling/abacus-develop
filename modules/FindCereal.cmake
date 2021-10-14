e0d60db41c63e163605b0ddddb6aa39e2e1267c9
Chun Cai <amoycaic@gmail.com>
2021-10-14 16:32:07 +0800

    Add: download cereal if not found.

--- modules/FindCereal.cmake
+++ modules/FindCereal.cmake
@@ -3,22 +3,27 @@
 # Find the native cereal headers.
 #
 #  CEREAL_FOUND - True if cereal is found.
-#  CEREAL_INCLUDE_DIRS - Where to find cereal headers.
+#  CEREAL_INCLUDE_DIR - Where to find cereal headers.
 #
 
 find_path(Cereal_INCLUDE_DIR
     cereal/cereal.hpp
-    HINTS ${CEREAL_INCLUDEDIR}
+    HINTS ${CEREAL_INCLUDE_DIR}
 )
 
+if(NOT Cereal_INCLUDE_DIR)
+    include(FetchContent)
+    FetchContent_Declare(
+        cereal
+        GIT_REPOSITORY https://github.com.cnpmjs.org/USCiLab/cereal.git
+        # URL https://github.com/USCiLab/cereal/archive/refs/tags/v1.3.0.tar.gz
+    )
+    set(Cereal_INCLUDE_DIR ${cereal_SOURCE_DIR})
+endif()
 # Handle the QUIET and REQUIRED arguments and
 # set Cereal_FOUND to TRUE if all variables are non-zero.
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Cereal DEFAULT_MSG Cereal_INCLUDE_DIR)
 
 # Copy the results to the output variables and target.
-if(Cereal_FOUND)
-    set(Cereal_INCLUDE_DIRS ${Cereal_INCLUDE_DIR})
-endif()
-
 mark_as_advanced(Cereal_INCLUDE_DIR)
