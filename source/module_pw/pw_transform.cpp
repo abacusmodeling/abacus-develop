3e115aaf4b1b05811c55deb1adc11affc4708d15
qianrui <Terry_Liu@pku.edu.cn>
2021-12-01 14:45:28 +0800

    change in-place fft to out-of-place

--- source/module_pw/pw_transform.cpp
+++ source/module_pw/pw_transform.cpp
@@ -21,13 +21,13 @@ void PW_Basis:: real2recip(std::complex<double> * in, std::complex<double> * out
     }
     this->ft.executefftw("2for");
 
-    this->gatherp_scatters(this->ft.c_rspace, this->ft.c_gspace);
+    this->gatherp_scatters(this->ft.c_rspace2, this->ft.c_gspace);
     
     this->ft.executefftw("1for");
 
     for(int ig = 0 ; ig < this->npw ; ++ig)
     {
-        out[ig] = this->ft.c_gspace[this->ig2isz[ig]];
+        out[ig] = this->ft.c_gspace2[this->ig2isz[ig]];
     }
     return;
 }
@@ -52,7 +52,7 @@ void PW_Basis:: real2recip(double * in, std::complex<double> * out)
 
     for(int ig = 0 ; ig < this->npw ; ++ig)
     {
-        out[ig] = this->ft.c_gspace[this->ig2isz[ig]];
+        out[ig] = this->ft.c_gspace2[this->ig2isz[ig]];
     }
     return;
 }
@@ -66,23 +66,20 @@ void PW_Basis:: recip2real(std::complex<double> * in, std::complex<double> * out
 {
     assert(this->gamma_only == false);
     ModuleBase::GlobalFunc::ZEROS(ft.c_gspace, this->nst * this->nz);
-    // for(int igg = 0 ; igg < this->nst * this->nz ; ++igg)
-    // {
-    //     this->ft.c_gspace[igg] = 0.0;
-    // }
+
     for(int ig = 0 ; ig < this->npw ; ++ig)
     {
         this->ft.c_gspace[this->ig2isz[ig]] = in[ig];
     }
     this->ft.executefftw("1bac");
 
-    this->gathers_scatterp(this->ft.c_gspace,this->ft.c_rspace);
+    this->gathers_scatterp(this->ft.c_gspace2,this->ft.c_rspace);
 
     this->ft.executefftw("2bac");
 
     for(int ir = 0 ; ir < this->nrxx ; ++ir)
     {
-        out[ir] = this->ft.c_rspace[ir] / this->bignxyz;
+        out[ir] = this->ft.c_rspace2[ir] / this->bignxyz;
     }
     return;
 }
@@ -96,17 +93,14 @@ void PW_Basis:: recip2real(std::complex<double> * in, double * out)
 {
     assert(this->gamma_only == true);
     ModuleBase::GlobalFunc::ZEROS(ft.c_gspace, this->nst * this->nz);
-    // for(int igg = 0 ; igg < this->nst * this->nz ; ++igg)
-    // {
-    //     this->ft.c_gspace[igg] = 0.0;
-    // }
+
     for(int ig = 0 ; ig < this->npw ; ++ig)
     {
         this->ft.c_gspace[this->ig2isz[ig]] = in[ig];
     }
     this->ft.executefftw("1bac");
     
-    this->gathers_scatterp(this->ft.c_gspace, this->ft.c_rspace);
+    this->gathers_scatterp(this->ft.c_gspace2, this->ft.c_rspace);
 
     this->ft.executefftw("2c2r");
 
