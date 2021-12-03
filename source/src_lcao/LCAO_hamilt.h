832396f5cb765e6b15deabd711452a2901799d88
linpz <linpz@mail.ustc.edu.cn>
2021-12-03 13:49:02 +0800

    1. add LCAO_Hamilt::calculate_HR_exx_sparse()

--- source/src_lcao/LCAO_hamilt.h
+++ source/src_lcao/LCAO_hamilt.h
@@ -29,6 +29,7 @@ class LCAO_Hamilt
     void calculat_HR_dftu_sparse(const int &current_spin, const double &sparse_threshold);
     void calculat_HR_dftu_soc_sparse(const int &current_spin, const double &sparse_threshold);
     void calculate_HSR_sparse(const int &current_spin, const double &sparse_threshold);
+    void calculate_HR_exx_sparse(const int &current_spin, const double &sparse_threshold);
     void clear_zero_elements(const int &current_spin, const double &sparse_threshold);
     void destroy_all_HSR_sparse(void);
 
