99071d694da7cbcb9eaf65f5caf3183deaba428f
maki49 <1579492865@qq.com>
2022-01-19 20:37:45 +0800

    add MPI_init

--- source/module_orbital/test/main.cpp
+++ source/module_orbital/test/main.cpp
@@ -1,8 +1,14 @@
 #include "gtest/gtest.h"
 int main(int argc, char** argv)
 {
-// test if the result of Center_2_Orb::Orb11:cal_overlap
-// is equal to the result of ORB_gen_table::snap_psipsi
-	testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+#ifdef __MPI
+    MPI_Init(&argc,&argv);
+#endif
+
+    testing::InitGoogleTest(&argc, argv);
+    int result = RUN_ALL_TESTS();
+
+#ifdef __MPI
+    MPI_Finalize();
+#endif
 }
