c44303c7c8ac71a0b108d39a7085ce799a8ddf12
hongriTianqi <z.hao.1@163.com>
2022-01-24 07:14:13 +0000

    modify tool_check_test.cpp

--- source/module_base/test/tool_check_test.cpp
+++ source/module_base/test/tool_check_test.cpp
@@ -1,44 +1,52 @@
 #include "../tool_check.h"
 #include "gtest/gtest.h"
-#include <fstream>
+#include "gmock/gmock.h"
 #include <cstdio>
+#include <fstream>
 
 /************************************************
-*  unit test of class ToolCheckTest
-***********************************************/
+ *  unit test of class ToolCheckTest
+ ***********************************************/
 
 /**
  * - Tested Function
  *   - ModuleBase::CHECK_NAME
- *	- check the next input from ifs is std::string
+ *    - check the next input from ifs is std::string
  *
  *   - ModuleBase::CHECK_INT
- *	- check the next input from ifs is int
+ *    - check the next input from ifs is int
  *
  *   - ModuleBase::CHECK_DOUBLE
- *	- check the next input from ifs is double
+ *    - check the next input from ifs is double
  *
  *   - ModuleBase::CHECK_STRING
- *	- check the next input from ifs is string
+ *    - check the next input from ifs is string
  *
  */
 
-
 class ToolCheckTest : public testing::Test
 {
-    protected:
-        std::ofstream ofs;
+  protected:
+	std::ofstream ofs;
 	std::ifstream ifs;
 	// define std::string, int, double variables
-	std::string name="abaqus";
+	std::string name = "abaqus";
 	int ecut = 100;
 	double occupation = 0.23;
-	std::string caltype="nscf";
+	std::string caltype = "nscf";
 	// quit is the swith to control performance of function
-	bool quit=false;
+	bool quit = false;
+	// for capturing stdout
+	std::string output = "";
+	void TearDown()
+	{
+		remove("tmp");
+	}
+
 };
 
-TEST_F(ToolCheckTest,Name){
+TEST_F(ToolCheckTest, Name)
+{
 	ofs.open("tmp");
 	// double input to check continus check function
 	ofs << name << std::endl;
@@ -46,13 +54,20 @@ TEST_F(ToolCheckTest,Name){
 	ofs.close();
 	ifs.open("tmp");
 	// non-quit check
-        ModuleBase::CHECK_NAME(ifs,"abacus",quit);
+	testing::internal::CaptureStdout();
+	ModuleBase::CHECK_NAME(ifs, "abacus", quit);
+	output = testing::internal::GetCapturedStdout();
+	EXPECT_THAT(output,testing::HasSubstr("not match"));
 	// quit check
-	EXPECT_DEATH(ModuleBase::CHECK_NAME(ifs,"abacus"),"");
+	testing::internal::CaptureStdout();
+	EXPECT_DEATH(ModuleBase::CHECK_NAME(ifs, "abacus"), "");
+	output = testing::internal::GetCapturedStdout();
+	EXPECT_THAT(output,testing::HasSubstr("NOTICE"));
 	ifs.close();
 }
 
-TEST_F(ToolCheckTest,Int){
+TEST_F(ToolCheckTest, Int)
+{
 	ofs.open("tmp");
 	// double input to check continus check function
 	ofs << ecut << std::endl;
@@ -60,13 +75,20 @@ TEST_F(ToolCheckTest,Int){
 	ofs.close();
 	ifs.open("tmp");
 	// non-quit check
-        ModuleBase::CHECK_INT(ifs,80,quit);
+	testing::internal::CaptureStdout();
+	ModuleBase::CHECK_INT(ifs, 80, quit);
+	output = testing::internal::GetCapturedStdout();
+	EXPECT_THAT(output,testing::HasSubstr("not match"));
 	// quit check
-	EXPECT_DEATH(ModuleBase::CHECK_INT(ifs,80),"");
+	testing::internal::CaptureStdout();
+	EXPECT_DEATH(ModuleBase::CHECK_INT(ifs, 80), "");
+	output = testing::internal::GetCapturedStdout();
+	EXPECT_THAT(output,testing::HasSubstr("NOTICE"));
 	ifs.close();
 }
 
-TEST_F(ToolCheckTest,Double){
+TEST_F(ToolCheckTest, Double)
+{
 	ofs.open("tmp");
 	// double input to check continus check function
 	ofs << occupation << std::endl;
@@ -74,13 +96,20 @@ TEST_F(ToolCheckTest,Double){
 	ofs.close();
 	ifs.open("tmp");
 	// non-quit check
-        ModuleBase::CHECK_DOUBLE(ifs,0.23002,quit);
+	testing::internal::CaptureStdout();
+	ModuleBase::CHECK_DOUBLE(ifs, 0.23002, quit);
+	output = testing::internal::GetCapturedStdout();
+	EXPECT_THAT(output,testing::HasSubstr("not match"));
 	// quit check
-	EXPECT_DEATH(ModuleBase::CHECK_DOUBLE(ifs,0.23002),"");
+	testing::internal::CaptureStdout();
+	EXPECT_DEATH(ModuleBase::CHECK_DOUBLE(ifs, 0.23002), "");
+	output = testing::internal::GetCapturedStdout();
+	EXPECT_THAT(output,testing::HasSubstr("NOTICE"));
 	ifs.close();
 }
 
-TEST_F(ToolCheckTest,String){
+TEST_F(ToolCheckTest, String)
+{
 	ofs.open("tmp");
 	// double input to check continus check function
 	ofs << caltype << std::endl;
@@ -88,11 +117,14 @@ TEST_F(ToolCheckTest,String){
 	ofs.close();
 	ifs.open("tmp");
 	// non-quit check
-        ModuleBase::CHECK_STRING(ifs,"scf",quit);
+	testing::internal::CaptureStdout();
+	ModuleBase::CHECK_STRING(ifs, "scf", quit);
+	output = testing::internal::GetCapturedStdout();
+	EXPECT_THAT(output,testing::HasSubstr("not match"));
 	// quit check
-	EXPECT_DEATH(ModuleBase::CHECK_STRING(ifs,"scf"),"");
+	testing::internal::CaptureStdout();
+	EXPECT_DEATH(ModuleBase::CHECK_STRING(ifs, "scf"), "");
+	output = testing::internal::GetCapturedStdout();
+	EXPECT_THAT(output,testing::HasSubstr("NOTICE"));
 	ifs.close();
-	// remove file tmp
-	remove("tmp");
 }
-
