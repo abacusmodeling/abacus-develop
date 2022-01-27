a1f53d18ba64fc2c88eac509639d611ed243ced6
xingliang <pxlppq007@163.com>
2022-01-27 10:45:52 +0800

    modify the comments according to the Doxygen standard

--- source/module_base/math_integral.h
+++ source/module_base/math_integral.h
@@ -20,8 +20,10 @@ class Integral
      * @param func [in] function to be integrated
      * @param rab [in] a list of interval
      * @param asum [out] the final integral value
+     * @author Peize Lin
+     * @date 2017-10-02
      */
-    static void Simpson_Integral  // Peize Lin accelerate 2017-10-02
+    static void Simpson_Integral 
     (    
         const int mesh,
         const double * const func,
@@ -36,8 +38,10 @@ class Integral
      * @param func [in] function to be integrated
      * @param dr [in] interval
      * @param asum [out] the final integral value
+     * @author Peize Lin
+     * @date 2017-10-02
      */
-	static void Simpson_Integral  //Peize Lin accelerate 2017-10-02
+	static void Simpson_Integral 
 	(
 		const int mesh,
 		const double * const func,
@@ -53,8 +57,10 @@ class Integral
      * @param func [in] function to be integrated
      * @param rab [in] a list of interval
      * @param asum [out] a list of integral value. asum[i] = integral from 0 to i. The max index of asum is an even (mesh-1 or mesh).
+     * @author Peize Lin
+     * @date 2016-02-14
      */
-    static void Simpson_Integral_0toall  //Peize Lin add 2016-02-14.
+    static void Simpson_Integral_0toall 
     (
         const int mesh,
         const double * const func,
@@ -69,8 +75,10 @@ class Integral
      * @param func [in] function to be integrated 
      * @param rab [in] r(i) * dr(i)/di * di or (b-a)/2n 
      * @param asum [out] a list of integral value. sum[i] = integral from i to mesh-1. 
+     * @author Peize Lin
+     * @date 2016-02-14
      */
-    static void Simpson_Integral_alltoinf //Peize Lin add 2016-02-14.
+    static void Simpson_Integral_alltoinf 
     (
         const int mesh,
         const double * const func,
