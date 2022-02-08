7a535a57f711bf1dd2fd90743a24d44af02cf430
maki49 <1579492865@qq.com>
2022-02-08 21:44:09 +0800

    modify CMakeLists.txt according to #676

--- source/module_orbital/test/1_snap_equal_test.cpp
+++ source/module_orbital/test/1_snap_equal_test.cpp
@@ -70,18 +70,3 @@ TEST_F(test_orb, equal_test)
 		}
 	}
 }
-
-int main(int argc, char** argv)
-{
-#ifdef __MPI
-    MPI_Init(&argc,&argv);
-#endif
-
-    testing::InitGoogleTest(&argc, argv);
-    int result = RUN_ALL_TESTS();
-
-#ifdef __MPI
-    MPI_Finalize();
-#endif
-	return result;
-}
