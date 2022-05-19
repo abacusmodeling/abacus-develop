b6fe08eb2374f1d7f44e5ee29b603c6a67cb4bf5
Chun Cai <amoycaic@gmail.com>
2022-05-19 12:48:00 +0800

    add debug info

--- examples/performance/run.sh
+++ examples/performance/run.sh
@@ -80,7 +80,7 @@ run_abacus() {
         lastword=$(tail -1 result.log | awk '{print $1}')
     fi
     if [[ $lastword != "SEE" ]]; then
-        /usr/bin/time -v mpirun -n $1 -env OMP_NUM_THREADS=$2 $abacus >result.log 2>time.log
+        /usr/bin/time -v mpirun -n $1 -env OMP_NUM_THREADS=$2 $abacus 2>time.log ｜ tee result.log
     else
         printf "**result.log is normal end, skip this job** "
     fi
