6087a1f1ec74f6b3c2c2436a9d200dc33d6d9f7b
ouqi0711 <ouqi@sas.upenn.edu>
2022-07-19 17:35:57 +0800

    add missed slater correlation part for PBE0

--- source/module_xc/xc_functional_wrapper_xc.cpp
+++ source/module_xc/xc_functional_wrapper_xc.cpp
@@ -6,6 +6,8 @@
 // 3. xc_spin_libxc, which is the wrapper for LDA functional, spin polarized
 
 #include "xc_functional.h"
+#include "../src_pw/global.h"
+#include "../module_base/global_function.h"
 #include <stdexcept>
 
 void XC_Functional::xc(const double &rho, double &exc, double &vxc)
@@ -33,8 +35,13 @@ void XC_Functional::xc(const double &rho, double &exc, double &vxc)
             // Exchange functionals containing attenuated slater exchange
             case XC_HYB_GGA_XC_PBEH:
             //  PBE0
-                XC_Functional::slater(rs, e, v);
-                e *= 0.75; v*= 0.75;
+                double ex, vx, ec, vc;
+                XC_Functional::slater(rs, ex, vx);
+                ex *= (1 - GlobalC::exx_global.info.hybrid_alpha); 
+                vx *= (1 - GlobalC::exx_global.info.hybrid_alpha);
+                XC_Functional::pw(rs, 0, ec, vc);
+                e = ex + ec;
+                v = vx + vc;
                 break;
 
             // Correlation functionals containing PW correlation
@@ -85,8 +92,15 @@ void XC_Functional::xc_spin(const double &rho, const double &zeta,
             // Exchange functionals containing attenuated slater exchange
             case XC_HYB_GGA_XC_PBEH:
             //  PBE0
-                XC_Functional::slater_spin(rho, zeta, e, vup, vdw);
-                e *= 0.75; vup *= 0.75; vdw *=0.75;
+                double ex, vupx, vdwx, ec, vupc, vdwc;
+                XC_Functional::slater_spin(rho, zeta, ex, vupx, vdwx);
+                ex *= (1.0 - GlobalC::exx_global.info.hybrid_alpha); 
+                vupx *= (1.0 - GlobalC::exx_global.info.hybrid_alpha); 
+                vdwx *= (1.0 - GlobalC::exx_global.info.hybrid_alpha);
+                XC_Functional::pz_spin(rs, zeta, ec, vupc, vdwc);
+                e = ex + ec;
+                vup = vupx + vupc;
+                vdw = vdwx + vdwc;
                 break;
 
             // Correlation functionals containing PZ correlation
