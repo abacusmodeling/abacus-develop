a07bdd68e925e97b1792bdb6d0085d637816400f
root <pxlppq007@163.com>
2022-07-28 16:58:51 +0800

    test: add `exit` in XXX_parallel_test.sh when running error

--- source/src_pdiag/test/diago_elpa_parallel_test.sh
+++ source/src_pdiag/test/diago_elpa_parallel_test.sh
@@ -9,6 +9,11 @@ for i in 6 3 2;do
     fi
     echo "TEST ELPA in parallel, nprocs=$i"
     mpirun -np $i ./hsolver_diago_elpa
+    if [[ $? != 0 ]];then
+        echo -e "\e[1;33m [  FAILED  ] \e[0m"\
+			"execute UT with $i cores error."
+        exit 1
+    fi
     break
 done
 
