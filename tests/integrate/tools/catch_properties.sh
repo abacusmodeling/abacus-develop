2761585005ef044d8c33b445ef8a7ea811196a09
Qianruipku <Terry_Liu@pku.edu.cn>
2022-07-29 08:32:33 +0800

    delete je-je.txt Chebycoef

--- tests/integrate/tools/catch_properties.sh
+++ tests/integrate/tools/catch_properties.sh
@@ -109,6 +109,7 @@ if ! test -z "$has_cond"  && [  $has_cond -eq 1 ]; then
 	oncal=Onsager.txt
 	python3 ../tools/CompareFile.py $onref $oncal 2
     echo "CompareH_Failed $?" >>$1
+	rm -f je-je.txt Chebycoef
 fi
 
 #echo total_dos
