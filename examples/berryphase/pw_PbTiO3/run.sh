6be31885e334e7d347126386ec3f531045d8b16c
pxlxingliang <91927439+pxlxingliang@users.noreply.github.com>
2022-09-22 11:20:01 +0800

    Docs: modify the files in examples of band/berryphase/bravias_lattice (#1264)
    * examples: modify the files in examples of band/berryphase/bravias_lattice
    
    * examples: delete the setting of 'pseudo_type' in INPUT in examples and performance
    
    * fix
    
    * examples: add band/pw_al example

--- examples/berryphase/pw_PbTiO3/run.sh
+++ examples/berryphase/pw_PbTiO3/run.sh
@@ -4,9 +4,10 @@ ABACUS_PATH=$(awk -F "=" '$1=="ABACUS_PATH"{print $2}' ../../SETENV)
 ABACUS_NPROCS=$(awk -F "=" '$1=="ABACUS_NPROCS"{print $2}' ../../SETENV)
 ABACUS_THREADS=$(awk -F "=" '$1=="ABACUS_THREADS"{print $2}' ../../SETENV)
 
-cp INPUT1 INPUT
+cp INPUT-scf INPUT
+cp KPT-scf KPT
 OMP_THREADS_NUM=${ABACUS_THREADS} mpirun -np ${ABACUS_NPROCS} ${ABACUS_PATH} | tee scf.output
-cp INPUT2 INPUT
-OMP_THREADS_NUM=${ABACUS_THREADS} mpirun -np ${ABACUS_NPROCS} ${ABACUS_PATH} | tee nscf.output
 
-rm INPUT
+cp INPUT-nscf-c INPUT
+cp KPT-nscf-c KPT
+OMP_THREADS_NUM=${ABACUS_THREADS} mpirun -np ${ABACUS_NPROCS} ${ABACUS_PATH} | tee nscf.output
--- examples/bravias_lattice/so_SnTe/run.sh
+++ examples/bravias_lattice/so_SnTe/run.sh
@@ -4,9 +4,4 @@ ABACUS_PATH=$(awk -F "=" '$1=="ABACUS_PATH"{print $2}' ../../SETENV)
 ABACUS_NPROCS=$(awk -F "=" '$1=="ABACUS_NPROCS"{print $2}' ../../SETENV)
 ABACUS_THREADS=$(awk -F "=" '$1=="ABACUS_THREADS"{print $2}' ../../SETENV)
 
-cp INPUT1 INPUT
-OMP_THREADS_NUM=${ABACUS_THREADS} mpirun -np ${ABACUS_NPROCS} ${ABACUS_PATH} | tee scf.output
-cp INPUT2 INPUT
-OMP_THREADS_NUM=${ABACUS_THREADS} mpirun -np ${ABACUS_NPROCS} ${ABACUS_PATH} | tee nscf.output
-
-rm INPUT
+OMP_THREADS_NUM=${ABACUS_THREADS} mpirun -np ${ABACUS_NPROCS} ${ABACUS_PATH} | tee output
