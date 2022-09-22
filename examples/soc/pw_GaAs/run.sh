4a1e93da6e99525904dd26689a10df92f79eb4a7
pxlxingliang <91927439+pxlxingliang@users.noreply.github.com>
2022-09-22 17:06:46 +0800

    examples: update example in examples/soc (#1282)

---
+++ examples/soc/pw_GaAs/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ABACUS_PATH=$(awk -F "=" '$1=="ABACUS_PATH"{print $2}' ../../SETENV)
+ABACUS_NPROCS=$(awk -F "=" '$1=="ABACUS_NPROCS"{print $2}' ../../SETENV)
+ABACUS_THREADS=$(awk -F "=" '$1=="ABACUS_THREADS"{print $2}' ../../SETENV)
+
+cp INPUT-scf INPUT
+cp KPT-scf KPT
+OMP_THREADS_NUM=${ABACUS_THREADS} mpirun -np ${ABACUS_NPROCS} ${ABACUS_PATH} | tee scf.output
+
+cp INPUT-nscf INPUT
+cp KPT-nscf KPT
+OMP_THREADS_NUM=${ABACUS_THREADS} mpirun -np ${ABACUS_NPROCS} ${ABACUS_PATH} | tee nscf.output
+
+rm INPUT KPT
