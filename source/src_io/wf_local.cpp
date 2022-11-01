91a7c1677b77e65f011fd93628aab9fcd93736e4
root <pxlppq007@163.com>
2022-11-01 11:16:08 +0800

    a test

--- source/src_io/wf_local.cpp
+++ source/src_io/wf_local.cpp
@@ -383,6 +383,7 @@ void WF_Local::write_lowf_complex(const std::string &name, std::complex<double> 
             {
                 if (j % 5 == 0) ofs << "\n";
                 ofs << ctot[i][j].real() << " " << ctot[i][j].imag() << " ";
+                GlobalV::ofs_running << ctot[i][j].real() << " " << ctot[i][j].imag() << " ";
             }
         }
         ofs.close();
