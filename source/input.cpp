dbe784c63be453835ef22a8775c5a52690bfa832
Tianqi Zhao <hongriTianqi@users.noreply.github.com>
2022-12-16 10:45:50 +0800

    Bug: fix a bug in count_ntype (#1692)

--- source/input.cpp
+++ source/input.cpp
@@ -3249,7 +3249,7 @@ int Input::count_ntype(const std::string &fn)
 		while(true)
 		{
 			ModuleBase::GlobalFunc::READ_VALUE(ifa, temp);
-			if (temp == "LATTICE_CONSTANT" || temp == "NUMERICAL_ORBITAL" || temp == "NUMERICAL_DESCRIPTOR")
+			if (temp == "LATTICE_CONSTANT" || temp == "NUMERICAL_ORBITAL" || temp == "NUMERICAL_DESCRIPTOR" || ifa.eof())
 			{
 				break;
 			}
