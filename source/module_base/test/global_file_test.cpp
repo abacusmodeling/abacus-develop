a6ad429a833bc4967b4844a49b204a50913dd258
yuhuan12 <120082969+yuhuan12@users.noreply.github.com>
2023-03-04 09:19:03 +0800

    Test:UT of openlog (#1989)

--- source/module_base/test/global_file_test.cpp
+++ source/module_base/test/global_file_test.cpp
@@ -17,6 +17,8 @@
  * - Tested Function
  * - mkdiratom
  *   - generate atom dir for each type of atom
+ * - openlog
+ *   - Open the out file with the name *.log
  */
 
 class GlobalFile : public testing::Test
@@ -30,4 +32,15 @@ TEST_F(GlobalFile,mkdiratom)
         ModuleBase::Global_File::make_dir_atom("Si");
         int a = access("./Si/",0);
         EXPECT_EQ(a , 0);
+}
+
+TEST_F(GlobalFile,openlog)
+{
+        std::ofstream ofs;
+        ModuleBase::Global_File::open_log(ofs,"Si","md",true);
+        EXPECT_TRUE(ofs.is_open());
+        ofs.close();
+        ModuleBase::Global_File::open_log(ofs,"Si","md",false);
+        EXPECT_TRUE(ofs.is_open());
+        ofs.close();
 }
