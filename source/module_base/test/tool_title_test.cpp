babfb08c63e454128daf1659936f8d3eadc2cf33
Mengyuan Wu <119288223+SeptemberMy@users.noreply.github.com>
2023-03-08 22:17:04 +0800

    Test:UnitTest for tool_title  (#2020)

---
+++ source/module_base/test/tool_title_test.cpp
@@ -0,0 +1,71 @@
+#include "../tool_title.h"
+#include "../global_variable.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+/************************************************
+ *  unit test of functions in tool_title.h
+ ***********************************************/
+
+/**
+ * - Tested Function
+ *   - ModuleBase::TITLE
+ *    - Output title for each function.
+ */
+
+class ToolTitleTest : public testing::Test
+{
+  protected:
+	std::ifstream ifs;
+    const std::string claname="ComplexMatrix";
+    const std::string funname="scaled_sum()";
+    const std::string cfname="ComplexMatrix::scaled_sum()";
+  void SetUp()
+        {
+        
+        }
+  void TearDown()
+	    {
+		  remove("TITLEtest1.log");
+          remove("TITLEtest2.log");
+          remove("TITLEtest3.log");
+	    }
+};
+
+TEST_F(ToolTitleTest, TITLE1)
+{
+    GlobalV::ofs_running.open("TITLEtest1.log");
+    std::string output1;
+    ModuleBase::TITLE(cfname,true);
+    GlobalV::ofs_running.close();
+    ifs.open("TITLEtest1.log");
+    getline(ifs,output1);
+    EXPECT_THAT(output1,testing::HasSubstr(" ==> ComplexMatrix::scaled_sum()"));
+    ifs.close(); 
+}
+
+TEST_F(ToolTitleTest, TITLE2)
+{
+    GlobalV::ofs_running.open("TITLEtest2.log");
+    std::string output2;
+    ModuleBase::TITLE(claname,funname,true);
+    GlobalV::ofs_running.close();
+    ifs.open("TITLEtest2.log");
+    getline(ifs,output2);
+    EXPECT_THAT(output2,testing::HasSubstr(" ==> ComplexMatrix::scaled_sum()"));
+    ifs.close();
+}
+
+TEST_F(ToolTitleTest, TITLE3)
+{
+    std::ofstream oofs;  
+    std::string output3a;
+    std::string output3b;
+    oofs.open("TITLEtest3.log");
+    ModuleBase::TITLE(oofs,claname,funname,true);
+    oofs.close();
+    ifs.open("TITLEtest3.log");
+    getline(ifs,output3a);
+    EXPECT_THAT(output3a,testing::HasSubstr(" ==> ComplexMatrix::scaled_sum()"));
+    ifs.close();
+}
--- source/module_hamilt_general/module_xc/test/xc3_mock.h
+++ source/module_hamilt_general/module_xc/test/xc3_mock.h
@@ -77,8 +77,8 @@ namespace ModuleBase
     IntArray::IntArray(int,int){};
     IntArray::~IntArray(){};
 
-    void TITLE(const std::string &class_function_name){};
-    void TITLE(const std::string &class_name,const std::string &function_name){};
+    void TITLE(const std::string &class_function_name,bool enable){};
+    void TITLE(const std::string &class_name,const std::string &function_name,bool enable){};
 }
 
 namespace GlobalV
