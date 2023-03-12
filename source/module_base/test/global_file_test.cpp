1d948d741e9a205eb05303c65c619b022b2f5360
yuhuan12 <120082969+yuhuan12@users.noreply.github.com>
2023-03-12 10:17:51 +0800

    Test:UT of mkdirout (#2050)

--- source/module_base/test/global_file_test.cpp
+++ source/module_base/test/global_file_test.cpp
@@ -26,6 +26,44 @@ class GlobalFile : public testing::Test
 
 };
 
+TEST_F(GlobalFile,mkdirout)
+{
+	    std::string output;
+	    testing::internal::CaptureStdout();
+	    ModuleBase::Global_File::make_dir_out("Si","m",false,0,true,true);
+	    output = testing::internal::GetCapturedStdout();
+	    EXPECT_THAT(output,testing::HasSubstr("MAKE THE DIR"));
+	    GlobalV::ofs_warning.close();
+	    EXPECT_TRUE(GlobalV::ofs_running.is_open());
+	    GlobalV::ofs_running.close();
+	    std::string dd = "OUT.Si/running_m_1.log";
+        remove(dd.c_str());
+
+	    testing::internal::CaptureStdout();
+	    ModuleBase::Global_File::make_dir_out("Si","md",false,0,true,false);
+	    output = testing::internal::GetCapturedStdout();
+	    EXPECT_THAT(output,testing::HasSubstr("MAKE THE STRU DIR"));
+	    EXPECT_TRUE(GlobalV::ofs_running.is_open());
+	    GlobalV::ofs_running.close();
+	    GlobalV::ofs_warning.close();
+	    std::string bb = "OUT.Si/running_md.log";
+        remove(bb.c_str());
+
+	    testing::internal::CaptureStdout();
+	    ModuleBase::Global_File::make_dir_out("Si","md",true,0,true,true);
+	    output = testing::internal::GetCapturedStdout();
+	    EXPECT_THAT(output,testing::HasSubstr("MAKE THE MATRIX DIR"));
+	    EXPECT_TRUE(GlobalV::ofs_running.is_open());
+	    GlobalV::ofs_running.close();
+	    std::string cc = "OUT.Si/running_md_1.log";
+        remove(cc.c_str());
+        std::string aa = "OUT.Si/warning.log";
+        remove(aa.c_str());
+        rmdir(GlobalV::global_stru_dir.c_str());
+        rmdir(GlobalV::global_matrix_dir.c_str());
+        rmdir(GlobalV::global_out_dir.c_str());
+}
+
 TEST_F(GlobalFile,mkdiratom)
 {
         GlobalV::global_out_dir = "./";
