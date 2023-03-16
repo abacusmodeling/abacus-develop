b7b21b537d4c72c531cd0b41d43ea39b1ca97b1f
Tianqi Zhao <hongriTianqi@users.noreply.github.com>
2023-03-16 10:37:48 +0800

    Test: fix a failed ut (#2084)

--- source/module_io/test/input_test.cpp
+++ source/module_io/test/input_test.cpp
@@ -351,10 +351,9 @@ TEST_F(InputTest, Default)
 TEST_F(InputTest, Read)
 {
 	std::string input_file = "./support/INPUT";
-	GlobalV::stru_file = "./support/STRU";
 	INPUT.Read(input_file);
 	EXPECT_EQ(INPUT.suffix,"autotest");
-	EXPECT_EQ(INPUT.stru_file,"STRU");
+	EXPECT_EQ(INPUT.stru_file,"./support/STRU");
 	EXPECT_EQ(INPUT.kpoint_file,"KPT");
 	EXPECT_EQ(INPUT.pseudo_dir,"../../PP_ORB/");
 	EXPECT_EQ(INPUT.orbital_dir,"../../PP_ORB/");
