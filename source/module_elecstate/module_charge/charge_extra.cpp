5823c9910d9f5538fcb73d0b3ef8239f29fd3906
Yu Liu <77716030+YuLiu98@users.noreply.github.com>
2023-04-13 15:09:21 +0800

    Fix: wrong rho in charge_extra.cpp (#2242)

--- source/module_elecstate/module_charge/charge_extra.cpp
+++ source/module_elecstate/module_charge/charge_extra.cpp
@@ -120,7 +120,7 @@ void Charge_Extra::extrapolate_charge(Charge* chr)
         
         ModuleBase::GlobalFunc::ZEROS(rho_atom[is], GlobalC::rhopw->nrxx);
     }
-    chr->atomic_rho(GlobalV::NSPIN, rho_atom, GlobalC::rhopw);
+    chr->atomic_rho(GlobalV::NSPIN, omega_old, rho_atom, GlobalC::rhopw);
 #ifdef _OPENMP
 #pragma omp parallel for collapse(2) schedule(static, 512)
 #endif
@@ -215,7 +215,7 @@ void Charge_Extra::extrapolate_charge(Charge* chr)
             ModuleBase::GlobalFunc::ZEROS(rho_atom[is] + irbeg, irlen);
         }
     });
-    chr->atomic_rho(GlobalV::NSPIN, rho_atom, GlobalC::rhopw);
+    chr->atomic_rho(GlobalV::NSPIN, GlobalC::ucell.omega, rho_atom, GlobalC::rhopw);
 #ifdef _OPENMP
 #pragma omp parallel for collapse(2) schedule(static, 512)
 #endif
