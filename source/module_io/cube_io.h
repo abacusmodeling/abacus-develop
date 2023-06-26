187ae9511a1ba55cc124f1861e9c612174b24a87
Yu Liu <77716030+YuLiu98@users.noreply.github.com>
2023-06-26 16:51:22 +0800

    Feature: enable the reading of mismatched charge density (#2668)
    * Feature: read-in chg when grids do not match
    
    * Test: add a test for reading in mismatched chg
    
    * Test: add UT for trilinear_interpolate()

--- source/module_io/cube_io.h
+++ source/module_io/cube_io.h
@@ -42,6 +42,50 @@ namespace ModuleIO
 		const UnitCell* ucell,
 		const int &precision = 11,
 		const int &out_fermi = 1);//mohan add 2007-10-17
+
+    /**
+     * @brief The trilinear interpolation method
+     *
+     * Trilinear interpolation is a method for interpolating grid data in 3D space.
+     * It estimates the value at a given position by interpolating the data along the three adjacent points.
+     *
+     * Specifically, for 3D grid data, trilinear interpolation requires determining the eight data points that are
+     * closest to the point where the estimation is required. These data points form a cube, with vertices at
+     * (x0,y0,z0), (x0+1,y0,z0), (x0,y0+1,z0), (x0+1,y0+1,z0), (x0,y0,z0+1), (x0+1,y0,z0+1), (x0,y0+1,z0+1) and
+     * (x0+1,y0+1,z0+1). Here, (x0,y0,z0) is the data point closest to the estimation point and has coordinate
+     * values less than those of the estimation point.
+     *
+     * For the estimation location (x,y,z), its estimated value in the grid can be calculated using the following
+     * formula: f(x,y,z) = f000(1-dx)(1-dy)(1-dz) + f100dx(1-dy)(1-dz) + f010(1-dx)dy(1-dz) +
+     *             f001(1-dx)(1-dy)dz + f101dx(1-dy)dz + f011(1-dx)dydz +
+     *             f110dxdy(1-dz) + f111dxdydz
+     * where fijk represents the data value at vertex i,j,k of the cube, and dx = x - x0, dy = y - y0, dz = z - z0
+     * represent the distance between the estimation point and the closest data point in each of the three
+     * directions, divided by the grid spacing. Here, it is assumed that the grid spacing is equal and can be
+     * omitted during computation.
+     *
+     * @param ifs the ifstream used to read charge density
+     * @param nx_read nx read from file
+     * @param ny_read ny read from file
+     * @param nz_read nz read from file
+     * @param nx the dimension of grids along x
+     * @param ny the dimension of grids along y
+     * @param nz the dimension of grids along z
+     * @param data the interpolated results
+     */
+    void trilinear_interpolate(std::ifstream& ifs,
+                               const int& nx_read,
+                               const int& ny_read,
+                               const int& nz_read,
+                               const int& nx,
+                               const int& ny,
+                               const int& nz,
+#ifdef __MPI
+                               double** data
+#else
+                               double* data
+#endif
+    );
 }
 
 #endif
--- source/module_io/read_cube.cpp
+++ source/module_io/read_cube.cpp
@@ -50,103 +50,198 @@ bool ModuleIO::read_cube(
 
 	ifs.ignore(150, '\n');
 
-	ModuleBase::CHECK_INT(ifs,ucell->nat,quit);
-	ifs.ignore(150, '\n');
+    ModuleBase::CHECK_INT(ifs, ucell->nat);
+    ifs.ignore(150, '\n');
 
-	double fac=ucell->lat0;
-	ModuleBase::CHECK_INT(ifs, nx);	
-	ModuleBase::CHECK_DOUBLE(ifs, fac*ucell->latvec.e11/double(nx), quit);
-	ModuleBase::CHECK_DOUBLE(ifs, fac*ucell->latvec.e12/double(nx), quit);
-	ModuleBase::CHECK_DOUBLE(ifs, fac*ucell->latvec.e13/double(nx), quit);
-	ModuleBase::CHECK_INT(ifs, ny);	
-	ModuleBase::CHECK_DOUBLE(ifs, fac*ucell->latvec.e21/double(ny), quit);
-	ModuleBase::CHECK_DOUBLE(ifs, fac*ucell->latvec.e22/double(ny), quit);
-	ModuleBase::CHECK_DOUBLE(ifs, fac*ucell->latvec.e23/double(ny), quit);
-	ModuleBase::CHECK_INT(ifs, nz);	
-	ModuleBase::CHECK_DOUBLE(ifs, fac*ucell->latvec.e31/double(nz), quit);
-	ModuleBase::CHECK_DOUBLE(ifs, fac*ucell->latvec.e32/double(nz), quit);
-	ModuleBase::CHECK_DOUBLE(ifs, fac*ucell->latvec.e33/double(nz), quit);
+    int nx_read = 0;
+    int ny_read = 0;
+    int nz_read = 0;
+    double fac = ucell->lat0;
+    ifs >> nx_read;
+    ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->latvec.e11 / double(nx), quit);
+    ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->latvec.e12 / double(nx), quit);
+    ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->latvec.e13 / double(nx), quit);
+    ifs >> ny_read;
+    ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->latvec.e21 / double(ny), quit);
+    ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->latvec.e22 / double(ny), quit);
+    ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->latvec.e23 / double(ny), quit);
+    ifs >> nz_read;
+    ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->latvec.e31 / double(nz), quit);
+    ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->latvec.e32 / double(nz), quit);
+    ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->latvec.e33 / double(nz), quit);
 
-	int temp = 0;
-	for(int it=0; it<ucell->ntype; it++)
-	{
-		for(int ia=0; ia<ucell->atoms[it].na; ia++)
-		{
-			ifs >> temp; // skip atomic number
-			ifs >> temp; // skip Z valance
-			// ModuleBase::CHECK_DOUBLE(ifs,GlobalC::ucell->atoms[it].ncpp.zv,quit); // check Z valance
-			ModuleBase::CHECK_DOUBLE(ifs,fac*ucell->atoms[it].tau[ia].x,quit);
-			ModuleBase::CHECK_DOUBLE(ifs,fac*ucell->atoms[it].tau[ia].y,quit);
-			ModuleBase::CHECK_DOUBLE(ifs,fac*ucell->atoms[it].tau[ia].z,quit);
-		}
-	}
+    const bool same = (nx == nx_read && ny == ny_read && nz == nz_read) ? true : false;
+
+    int temp = 0;
+    for (int it = 0; it < ucell->ntype; it++)
+    {
+        for (int ia = 0; ia < ucell->atoms[it].na; ia++)
+        {
+            ifs >> temp; // skip atomic number
+            ifs >> temp; // skip Z valance
+            ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->atoms[it].tau[ia].x, quit);
+            ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->atoms[it].tau[ia].y, quit);
+            ModuleBase::CHECK_DOUBLE(ifs, fac * ucell->atoms[it].tau[ia].z, quit);
+        }
+    }
 
 #ifdef __MPI
-	const int nxy = nx * ny;
-	double *zpiece = nullptr;
-	double **tempRho = nullptr;
+    const int nxy = nx * ny;
+    double* zpiece = nullptr;
+    double** read_rho = nullptr;
+    if (GlobalV::MY_RANK == 0 || (GlobalV::ESOLVER_TYPE == "sdft" && GlobalV::RANK_IN_STOGROUP == 0))
+    {
+        read_rho = new double*[nz];
+        for (int iz = 0; iz < nz; iz++)
+        {
+            read_rho[iz] = new double[nxy];
+        }
+        if (same)
+        {
+            for (int ix = 0; ix < nx; ix++)
+            {
+                for (int iy = 0; iy < ny; iy++)
+                {
+                    for (int iz = 0; iz < nz; iz++)
+                    {
+                        ifs >> read_rho[iz][ix * ny + iy];
+                    }
+                }
+            }
+        }
+        else
+        {
+            ModuleIO::trilinear_interpolate(ifs, nx_read, ny_read, nz_read, nx, ny, nz, read_rho);
+        }
+    }
+    else
+    {
+        zpiece = new double[nxy];
+        ModuleBase::GlobalFunc::ZEROS(zpiece, nxy);
+    }
 
-	if(GlobalV::MY_RANK==0||(GlobalV::ESOLVER_TYPE == "sdft"&&GlobalV::RANK_IN_STOGROUP==0))
-	{
-		tempRho = new double*[nz];
-		for(int iz=0; iz<nz; iz++)
-		{
-			tempRho[iz] = new double[nxy];
-			// ModuleBase::GlobalFunc::ZEROS(tempRho[iz], nxy);
-		}
-		for(int ix=0; ix<nx; ix++)
-		{
-			for(int iy=0; iy<ny; iy++)
-			{
-				for(int iz=0; iz<nz; iz++)
-				{
-					ifs >> tempRho[iz][ix*ny + iy];
-				}
-			}
-		}
-	}
-	else
-	{
-		zpiece = new double[nxy];
-		ModuleBase::GlobalFunc::ZEROS(zpiece, nxy);
-	}
+    for (int iz = 0; iz < nz; iz++)
+    {
+        if (GlobalV::MY_RANK == 0 || (GlobalV::ESOLVER_TYPE == "sdft" && GlobalV::RANK_IN_STOGROUP == 0))
+        {
+            zpiece = read_rho[iz];
+        }
+        Pgrid->zpiece_to_all(zpiece, iz, data);
+    } // iz
 
-	for(int iz=0; iz<nz; iz++)
-	{
-		if(GlobalV::MY_RANK==0||(GlobalV::ESOLVER_TYPE == "sdft"&&GlobalV::RANK_IN_STOGROUP==0))
-		{
-			zpiece = tempRho[iz];
-		}
-		Pgrid->zpiece_to_all(zpiece, iz, data);
-	}// iz
-
-	if(GlobalV::MY_RANK==0||(GlobalV::ESOLVER_TYPE == "sdft"&&GlobalV::RANK_IN_STOGROUP==0))
-	{
-		for(int iz=0; iz<nz; iz++)
-		{
-			delete[] tempRho[iz];
-		}
-		delete[] tempRho;
-	}
-	else
-	{
-		delete[] zpiece;
-	}
+    if (GlobalV::MY_RANK == 0 || (GlobalV::ESOLVER_TYPE == "sdft" && GlobalV::RANK_IN_STOGROUP == 0))
+    {
+        for (int iz = 0; iz < nz; iz++)
+        {
+            delete[] read_rho[iz];
+        }
+        delete[] read_rho;
+    }
+    else
+    {
+        delete[] zpiece;
+    }
 #else
-	GlobalV::ofs_running << " Read SPIN = " << is+1 << " charge now." << std::endl;
-	// consistent with the write_cube,
-	for(int i=0; i<nx; i++)
-	{
-		for(int j=0; j<ny; j++)
-		{
-			for(int k=0; k<nz; k++)
-			{
-				ifs >> data[k*nx*ny+i*ny+j];
-			}
-		}
-	}
+    GlobalV::ofs_running << " Read SPIN = " << is + 1 << " charge now." << std::endl;
+    if (same)
+    {
+        for (int i = 0; i < nx; i++)
+        {
+            for (int j = 0; j < ny; j++)
+            {
+                for (int k = 0; k < nz; k++)
+                {
+                    ifs >> data[k * nx * ny + i * ny + j];
+                }
+            }
+        }
+    }
+    else
+    {
+        ModuleIO::trilinear_interpolate(ifs, nx_read, ny_read, nz_read, nx, ny, nz, data);
+    }
 #endif
 
-    if(GlobalV::MY_RANK==0||(GlobalV::ESOLVER_TYPE == "sdft"&&GlobalV::RANK_IN_STOGROUP==0)) ifs.close();
+    if (GlobalV::MY_RANK == 0 || (GlobalV::ESOLVER_TYPE == "sdft" && GlobalV::RANK_IN_STOGROUP == 0))
+        ifs.close();
     return true;
+}
+
+void ModuleIO::trilinear_interpolate(std::ifstream& ifs,
+                                     const int& nx_read,
+                                     const int& ny_read,
+                                     const int& nz_read,
+                                     const int& nx,
+                                     const int& ny,
+                                     const int& nz,
+#ifdef __MPI
+                                     double** data
+#else
+                                     double* data
+#endif
+)
+{
+    ModuleBase::TITLE("ModuleIO", "trilinear_interpolate");
+
+    double** read_rho = new double*[nz_read];
+    for (int iz = 0; iz < nz_read; iz++)
+    {
+        read_rho[iz] = new double[nx_read * ny_read];
+    }
+    for (int ix = 0; ix < nx_read; ix++)
+    {
+        for (int iy = 0; iy < ny_read; iy++)
+        {
+            for (int iz = 0; iz < nz_read; iz++)
+            {
+                ifs >> read_rho[iz][ix * ny_read + iy];
+            }
+        }
+    }
+
+    for (int ix = 0; ix < nx; ix++)
+    {
+        double fracx = 0.5 * (static_cast<double>(nx_read) / nx * (1.0 + 2.0 * ix) - 1.0);
+        fracx = std::fmod(fracx, nx_read);
+        int lowx = static_cast<int>(fracx);
+        double dx = fracx - lowx;
+        int highx = (lowx == nx_read - 1) ? 0 : lowx + 1; // the point nz_read is the same as 0
+        for (int iy = 0; iy < ny; iy++)
+        {
+            double fracy = 0.5 * (static_cast<double>(ny_read) / ny * (1.0 + 2.0 * iy) - 1.0);
+            fracy = std::fmod(fracy, ny_read);
+            int lowy = static_cast<int>(fracy);
+            double dy = fracy - lowy;
+            int highy = (lowy == ny_read - 1) ? 0 : lowy + 1;
+            for (int iz = 0; iz < nz; iz++)
+            {
+                double fracz = 0.5 * (static_cast<double>(nz_read) / nz * (1.0 + 2.0 * iz) - 1.0);
+                fracz = std::fmod(fracz, nz_read);
+                int lowz = static_cast<int>(fracz);
+                double dz = fracz - lowz;
+                int highz = (lowz == nz_read - 1) ? 0 : lowz + 1;
+
+                double result = read_rho[lowz][lowx * ny_read + lowy] * (1 - dx) * (1 - dy) * (1 - dz)
+                                + read_rho[lowz][highx * ny_read + lowy] * dx * (1 - dy) * (1 - dz)
+                                + read_rho[lowz][lowx * ny_read + highy] * (1 - dx) * dy * (1 - dz)
+                                + read_rho[highz][lowx * ny_read + lowy] * (1 - dx) * (1 - dy) * dz
+                                + read_rho[lowz][highx * ny_read + highy] * dx * dy * (1 - dz)
+                                + read_rho[highz][highx * ny_read + lowy] * dx * (1 - dy) * dz
+                                + read_rho[highz][lowx * ny_read + highy] * (1 - dx) * dy * dz
+                                + read_rho[highz][highx * ny_read + highy] * dx * dy * dz;
+
+#ifdef __MPI
+                data[iz][ix * ny + iy] = result;
+#else
+                data[iz * nx * ny + ix * ny + iy] = result;
+#endif
+            }
+        }
+    }
+
+    for (int iz = 0; iz < nz_read; iz++)
+    {
+        delete[] read_rho[iz];
+    }
+    delete[] read_rho;
 }
