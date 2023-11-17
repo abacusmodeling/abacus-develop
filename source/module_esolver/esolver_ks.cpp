5f6a67e1747d7e3b0ef6f5589539b55a059baed0
Wenfei Li <38569667+wenfei-li@users.noreply.github.com>
2023-11-17 17:01:01 +0800

    Fix : two bugs in PAW (#3217)
    * update version of libpaw
    
    * Fix : paw bug when compiled serial
    
    ---------
    
    Co-authored-by: wenfei-li <liwenfei@gmail.com>

--- source/module_esolver/esolver_ks.cpp
+++ source/module_esolver/esolver_ks.cpp
@@ -261,7 +261,7 @@ namespace ModuleESolver
                 }  
             }
 #else
-            this->get_rhoijp(rhoijp, rhoijselect, nrhoijsel);
+            GlobalC::paw_cell.get_rhoijp(rhoijp, rhoijselect, nrhoijsel);
 
             for(int iat = 0; iat < GlobalC::ucell.nat; iat ++)
             {
