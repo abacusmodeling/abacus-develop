796ca1c9a43b61ecede858da7bf51ef16fb53236
wqzhou <33364058+WHUweiqingzhou@users.noreply.github.com>
2024-01-10 11:08:33 +0800

    Feature: Autoset a finite magmon if STRU does not specify a finite one (#3453)
    * Autoset a finite magmon if STRU does not specify a finite one for nspin=4||npsin=4 calculations
    
    * fix 207_NO_KP_OHS_SPIN4
    
    * fix 140_PW_15_SO
    
    * move AutoSet after ntype-cycle
    
    * modify stru.md
    
    * change result.ref of 207_NO_KP_OHS_SPIN4

--- source/module_cell/read_atoms.cpp
+++ source/module_cell/read_atoms.cpp
@@ -709,7 +709,6 @@ bool UnitCell::read_atom_positions(std::ifstream &ifpos, std::ofstream &ofs_runn
 							ModuleBase::GlobalFunc::OUT(ofs_running, ss.str(),atoms[it].mag[ia]);
 						}
 					}
-
 			
 					if(Coordinate=="Direct")
 					{
@@ -804,7 +803,51 @@ bool UnitCell::read_atom_positions(std::ifstream &ifpos, std::ofstream &ofs_runn
                 magnet.start_magnetization[it] = 0.0;
             }
         } // end for ntype
-    }     // end scan_begin
+		// Start Autoset magnetization
+		// defaultly set a finite magnetization if magnetization is not specified
+		int autoset_mag = 1;
+		for (int it = 0;it < ntype; it++)
+		{
+			for (int ia = 0;ia < this->atoms[it].na; ia++)
+			{
+				if(std::abs(atoms[it].mag[ia]) > 1e-5)
+				{
+					autoset_mag = 0;
+					break;
+				}
+			}
+		}
+		if (autoset_mag)
+		{
+			if(GlobalV::NSPIN==4)
+			{
+				for (int it = 0;it < ntype; it++)
+				{
+					for (int ia = 0;ia < this->atoms[it].na; ia++)
+					{
+						atoms[it].m_loc_[ia].x = 1.0;
+						atoms[it].m_loc_[ia].y = 1.0;
+						atoms[it].m_loc_[ia].z = 1.0;
+						atoms[it].mag[ia] = sqrt(pow(atoms[it].m_loc_[ia].x,2)+pow(atoms[it].m_loc_[ia].y,2)+pow(atoms[it].m_loc_[ia].z,2));
+						ModuleBase::GlobalFunc::OUT(ofs_running,"Autoset magnetism for this atom", 1.0, 1.0, 1.0);
+					}
+				}
+			}
+			else if(GlobalV::NSPIN==2)
+			{
+				for (int it = 0;it < ntype; it++)
+				{
+					for (int ia = 0;ia < this->atoms[it].na; ia++)
+					{
+						atoms[it].mag[ia] = 1.0;
+						atoms[it].m_loc_[ia].x = atoms[it].mag[ia];
+						ModuleBase::GlobalFunc::OUT(ofs_running,"Autoset magnetism for this atom", 1.0);
+					}
+				}
+			}
+		}
+		// End Autoset magnetization
+    }   // end scan_begin
 
 //check if any atom can move in MD
 	if(!this->if_atoms_can_move() && GlobalV::CALCULATION=="md" && GlobalV::ESOLVER_TYPE!="tddft")
