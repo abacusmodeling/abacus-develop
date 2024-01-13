781e5330757056899a493bad14611ba5401ae1ca
Denghui Lu <denghuilu@pku.edu.cn>
2024-01-13 17:40:51 +0800

    remove backward-cpp tools (#3480)

--- source/module_base/module_container/base/utils/logging.h
+++ source/module_base/module_container/base/utils/logging.h
@@ -2,7 +2,6 @@
 #define BASE_CORE_LOGGING_H_
 
 #include <base/macros/macros.h>
-#include <base/third_party/backward-cpp/backward.hpp>
 
 namespace base {
 namespace utils {
@@ -15,8 +14,6 @@ inline static const char* check_msg_impl(const char* msg) {
 
 inline static void check_exit_impl(const char* func, const char* file, uint32_t line, const char* msg) {
     fprintf(stderr, "Fatal error in function %s, file %s, line %u, \nwith message: \n\t\t%s\n", func, file, line, msg);
-    backward::StackTrace st; st.load_here(32); st.skip_n_firsts(3);
-    backward::Printer p; p.print(st);
     std::abort();
 }
 
