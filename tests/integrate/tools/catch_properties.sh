f16c4a74dbdcced1bfa0197f569b6694bc87af3d
Yu Liu <77716030+YuLiu98@users.noreply.github.com>
2024-06-14 19:36:12 -0700

    Test: accelerate integrate tests (#4358)
    * Test: accelerate integrate tests
    
    * Test: update 204_NO_NC_magnetic
    
    * Test: update tests

--- tests/integrate/tools/catch_properties.sh
+++ tests/integrate/tools/catch_properties.sh
@@ -316,7 +316,7 @@ if ! test -z "$has_lowf"  && [ $has_lowf == 1 ]; then
 		wfc_ref=WFC_NAO_GAMMA1.txt.ref	
 	else
 		if ! test -z "$out_app_flag"  && [ $out_app_flag == 0 ]; then
-			wfc_name=WFC_NAO_K1_ION11
+			wfc_name=WFC_NAO_K1_ION3
 		else
 			wfc_name=WFC_NAO_K2
 		fi
