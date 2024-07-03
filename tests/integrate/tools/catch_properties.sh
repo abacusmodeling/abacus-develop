048567fbe89e0b4e887695bb56f3bf462f2b8688
LUNASEA <33978601+maki49@users.noreply.github.com>
2024-07-03 21:34:34 +0800

    Fix test for output Vxc (#4542)
    * fix xc test
    
    * fix catch_properties
    
    * Update digits according to the lower-precision parameter

--- tests/integrate/tools/catch_properties.sh
+++ tests/integrate/tools/catch_properties.sh
@@ -231,8 +231,9 @@ if ! test -z "$has_xc"  && [  $has_xc == 1 ]; then
 	oeref=vxc_out.ref
 	oecal=OUT.autotest/vxc_out
 	python3 ../tools/CompareFile.py $xcref $xccal 4
-	python3 ../tools/CompareFile.py $oeref $oecal 6
-    echo "CompareXC_pass $?" >>$1
+	echo "CompareXC_pass $?" >>$1
+	python3 ../tools/CompareFile.py $oeref $oecal 5
+    echo "CompareOE_pass $?" >>$1
 fi
 
 #echo $has_hs2
