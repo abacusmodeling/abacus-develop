4330ec52563559cb037ba150e7cb77a544b9a866
root <13661144349@163.com>
2026-03-22 11:03:32 +0800

    debug

--- source/source_hsolver/module_genelpa/utils.cpp
+++ source/source_hsolver/module_genelpa/utils.cpp
@@ -91,9 +91,13 @@ void loadMatrix(const char FileName[], int nFull, double* a, int* desca, int bla
 
     const int ROOT_PROC = 0;
     std::ifstream matrixFile;
-    if (myid == ROOT_PROC)
-        matrixFile.open(FileName);
-
+    if (myid == ROOT_PROC) {
+    matrixFile.open(FileName);
+    if (!matrixFile.is_open()) {
+        std::cerr << "Error: Failed to open file \"" << FileName << "\"." << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
     double* b; // buffer
     const int MAX_BUFFER_SIZE = 1e9; // max buffer size is 1GB
 
