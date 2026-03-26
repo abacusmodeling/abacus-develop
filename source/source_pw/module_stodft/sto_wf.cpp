e26d42a906852250b7e054aca445881d7ddb4159
dyzheng <zhengdy@bjaisi.com>
2026-03-26 22:53:19 +0800

    Refactor: Replace magic number and C-style cast in sto_wf.cpp

--- source/source_pw/module_stodft/sto_wf.cpp
+++ source/source_pw/module_stodft/sto_wf.cpp
@@ -66,13 +66,15 @@ void Stochastic_WF<T, Device>::clean_chiallorder()
 template <typename T, typename Device>
 void Stochastic_WF<T, Device>::init_sto_orbitals(const int seed_in)
 {
+       const unsigned int RANK_SEED_FACTOR = 10000; // 用于区分不同进程/组的随机种子偏移量
+
     if (seed_in == 0 || seed_in == -1)
     {
-        srand((unsigned)time(nullptr) + GlobalV::MY_RANK * 10000); // GlobalV global variables are reserved
+        srand(static_cast<unsigned>(time(nullptr)) + GlobalV::MY_RANK * RANK_SEED_FACTOR);
     }
     else
     {
-        srand((unsigned)std::abs(seed_in) + (GlobalV::MY_BNDGROUP * GlobalV::NPROC_IN_BNDGROUP + GlobalV::RANK_IN_BPGROUP) * 10000);
+        srand(static_cast<unsigned>(std::abs(seed_in)) + (GlobalV::MY_BNDGROUP * GlobalV::NPROC_IN_BNDGROUP + GlobalV::RANK_IN_BPGROUP) * RANK_SEED_FACTOR);
     }
 
     this->allocate_chi0();
